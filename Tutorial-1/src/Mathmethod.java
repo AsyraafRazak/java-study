0874f28e0c18d93cae9b00ea5adba30c852ebca2
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2024-10-25 16:50:13 +0800

    Math

---
+++ Tutorial-1/src/Mathmethod.java
@@ -0,0 +1,11 @@
+public class Mathmethod {
+    public static void main(String[] args) throws Exception {
+    
+        System.out.println(Math.max(5, 10)); //highest value of x and y
+        System.out.println(Math.min(5, 10)); //lowest value of x and y
+        System.out.println(Math.sqrt(64)); //square root of x
+        System.out.println(Math.abs(-4.7)); //absolute (positive) value of x
+        System.out.println(Math.random()); //random number between 0.0 (inclusive), and 1.0 (exclusive)
+
+    }
+}
